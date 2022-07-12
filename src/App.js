8c03889f5fa7e84753f7b91f40773e29aeb00c5a
peterdez <adebanjopete@gmail.com>
2022-07-12 22:19:32 +0100

    Changed heading

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App(props) {
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>ReactTodo</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
          {filterList}
